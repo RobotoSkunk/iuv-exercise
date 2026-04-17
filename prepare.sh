6358516a2dda6f56530264ad9ebcbfed5bdf4415
RobotoSkunk <contact@robotoskunk.com>
2026-04-16 23:03:07 -0600

    Updated prepare.sh
    The script has been updated to be more flexible when preparing
    modules packages.

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,15 @@
 
 CURRENT_DIRECTORY="$(pwd)";
 
-cd modules/frontend;
-npm i;
+function install_dependencies
+{
+	cd "$CURRENT_DIRECTORY/modules/$1";
+	npm i;
+};
+
+install_dependencies frontend
+install_dependencies database
+install_dependencies checkout
+install_dependencies authentication
 
 cd $CURRENT_DIRECTORY;
