d675f9a27bb376c300f2ec40794c478887439882
RobotoSkunk <contact@robotoskunk.com>
2026-05-15 07:27:18 -0600

    Added start.sh script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Compilando módulo de base de datos..."
+cd modules/database
+tsc
+
+echo "Compilando módulo de chequeos..."
+cd ../checkout
+tsc
+
+echo "Compilando módulo de autenticación..."
+cd ../authentication
+tsc
+
+echo "Compilando módulo del cliente..."
+cd ../frontend
+npm run build
+
+echo "Iniciando..."
+cd ../..
+pm2 start
