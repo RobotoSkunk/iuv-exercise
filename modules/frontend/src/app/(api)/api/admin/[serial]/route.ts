6a44b3087f3940e19384ccdb95e845f032e02c82
RobotoSkunk <contact@robotoskunk.com>
2026-05-15 04:37:08 -0600

    Added update administrators

--- modules/frontend/src/app/(api)/api/admin/[serial]/route.ts
+++ modules/frontend/src/app/(api)/api/admin/[serial]/route.ts
@@ -47,3 +47,85 @@ export async function DELETE(_: NextRequest, {
 		code: 0,
 	});
 }
+
+const required: {
+	[ key: string ]: string;
+} = {
+	name: 'string',
+	lastname_father: 'string',
+	lastname_mother: 'string',
+	role_id: 'number',
+};
+
+export async function PATCH(request: NextRequest, {
+	params,
+}: {
+	params: Promise<{ serial: number }>;
+})
+{
+	const { serial } = await params;
+
+	try {
+		const data = await request.json() as Partial<AdminData> & { [ key: string ]: string | number | undefined };
+
+		for (const key in required) {
+			const type = typeof data[key];
+			const reqType = required[key];
+
+			if (type === 'undefined') {
+				continue;
+			}
+
+			if (type !== reqType) {
+				return Response.json({
+					code: -1,
+					error: `Expected '${key}' of type '${reqType}', but got '${type}'.`,
+				});
+
+			// @ts-ignore The type is alredy being checked
+			} else if (type === 'string' && data[key].length === 0) {
+				return Response.json({
+					code: -2,
+					error: `The content of '${key}' is missing.`,
+				});
+
+			// @ts-ignore The type is alredy being checked
+			} else if (type === 'number' && data[key] < 0) {
+				return Response.json({
+					code: -3,
+					error: `The content of '${key}' is missing.`,
+				});
+			}
+		}
+
+		const req = await fetch(`http://127.0.0.1:5001/administrator/${serial}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+
+		const json = await req.json() as { code: number, error?: string };
+
+		if (json.code !== 0) {
+			console.error(json.error);
+
+			return Response.json({
+				code: -4,
+				error: `Algo salió mal, intenta de nuevo más tarde.`,
+			});
+		}
+
+		return Response.json({
+			code: 0,
+		});
+	} catch (error) {
+		console.error(error);
+
+		return Response.json({
+			code: -5,
+			error: `Algo salió mal, intenta de nuevo más tarde.`,
+		});
+	}
+}
--- modules/frontend/src/app/(main)/administrador/[serial]/page.tsx
+++ modules/frontend/src/app/(main)/administrador/[serial]/page.tsx
@@ -3,15 +3,24 @@
 
 import {
 	use,
+	useContext,
 	useEffect,
 	useState,
 } from 'react';
+
+import {
+	NotificationsContext,
+} from '@/contexts/notifications';
 
 import panelStyle from '../../panel.module.css';
 
 type Admin = Omit<AdminData, 'role_id'> & {
 	id: number;
 	name: string;
+	role: {
+		id: number;
+		name: 'string';
+	},
 };
 
 export default function Page({
@@ -21,10 +30,24 @@ export default function Page({
 })
 {
 	const { serial } = use(params);
+	const notificationsContext = useContext(NotificationsContext);
+
+	const [ busy, setBusy ] = useState<boolean>(false);
 	const [ adminData, setAdminData ] = useState<Admin | null>(null);
+	const [ roles, setRoles ] = useState<RoleData[]>([]);
+
+	async function fetchRoles()
+	{
+		const result = await fetch('/api/roles');
+		const json = await result.json() as APIResponse<RoleData[]>;
+
+		setRoles(json.data);
+	}
 
 	useEffect(() => {
 		(async () => {
+			await fetchRoles();
+
 			const result = await fetch(`/api/admin/${serial}`);
 			const json = await result.json() as APIResponse<Admin>;
 
@@ -40,10 +63,55 @@ export default function Page({
 		<h1 className={ panelStyle.title }>Información del Administrador</h1>
 		<form
 			className={ panelStyle.form }
-			onSubmit={ (ev) => {
+			onSubmit={ async (ev) => {
 				ev.preventDefault();
 
-				
+				if (busy) {
+					return;
+				}
+
+				const form = ev.currentTarget;
+
+				if (!form.checkValidity()) {
+					form.reportValidity();
+					return;
+				}
+
+				setBusy(true);
+
+				const formData = new FormData(ev.currentTarget);
+				const data: { [ key: string ]: string | number } = { };
+
+				for (const [ key, value ] of formData.entries()) {
+					if (key === 'role_id') {
+						data[key] = Number.parseInt(value as string);
+					} else {
+						data[key] = value as string;
+					}
+				}
+
+				try {
+					const response = await fetch(`/api/admin/${serial}`, {
+						method: 'PATCH',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify(data),
+					});
+
+					const json = await response.json() as { code: number, error?: string };
+
+					if (!json.error) {
+						notificationsContext.push('success', 'Datos actualizados exitosamente.');
+					} else {
+						alert(json.error);
+					}
+				} catch (error) {
+					alert('Algo ha salido mal, intenta de nuevo más tarde');
+					console.error(error);
+				} finally {
+					setBusy(false);
+				}
 			} }
 		>
 			<label className={ panelStyle['input-label'] }>
@@ -59,7 +127,22 @@ export default function Page({
 				<input type='text' name='lastname-mother' defaultValue={ adminData.lastname_mother }/>
 			</label>
 
+			<select name='role_id' defaultValue={ adminData.role.id }>
+				{ roles.map((v, i) => (
+					<option
+						key={ i }
+						value={ v.id }
+					>
+						{ v.name }
+					</option>
+				)) }
+			</select>
+
 			<button>Actualizar datos</button>
 		</form>
+		<section style={{ textAlign: 'center' }}>
+			<h2>Registro de auditoría</h2>
+			<p>Próximamente, parte del tercer sprint.</p>
+		</section>
 	</>);
 };
